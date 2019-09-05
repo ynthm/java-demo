2cb6783b0648ab3e06b51cac8f19cdd147f04515
Ynthm Wang <ynthm.w@gmail.com>
2019-09-05 14:36:50 +0800

    first step

---
+++ test/src/main/java/com/ynthm/test/UserService.java
@@ -0,0 +1,9 @@
+package com.ynthm.test;
+
+/**
+ * Author : Ynthm
+ */
+public interface UserService {
+
+    boolean saveUser(User user);
+}
---
+++ test/src/main/java/com/ynthm/test/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ynthm.test;
+
+/**
+ * Author : Ynthm
+ */
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public boolean saveUser(User user) {
+        System.out.println("保存用户: " + user.getName());
+        return true;
+    }
+}
